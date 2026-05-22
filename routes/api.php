15bd44c61035fa8f56fd118b18e797f1b658c3dc
Rajni Sharma <rajni@bitpixelcoders.com>
2026-05-22 18:24:45 +0530

    changes

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\WidgetConversationController;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/widget/conversation/start', [WidgetConversationController::class, 'start']);
-Route::post('/widget/conversation/end', [WidgetConversationController::class, 'end']);
--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::redirect('/login', '/admin/login');
-Route::redirect('/', '/admin');
